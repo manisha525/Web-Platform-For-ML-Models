c1dc433c3ca9746467df4c0fb8932cdb42be6724
manisha525 <59557054+manisha525@users.noreply.github.com>
2021-08-09 12:31:25 +0545

    Add files via upload

---
+++ app.py
@@ -0,0 +1,129 @@
+from flask import Flask,render_template
+import joblib
+import pickle
+from flask import request
+import numpy as np
+import os
+
+app=Flask(__name__,template_folder='templates')
+flag=1
+dir_path = os.path.dirname(os.path.realpath(__file__))
+model4=pickle.load(open('forest.pkl','rb'))
+@app.route("/")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+# @app.route("/cancer")
+# def cancer():
+#     return render_template("cancer.html")
+
+
+@app.route("/diabetes")
+def diabetes():
+    return render_template("diabetes.html")
+
+@app.route("/heart")
+def heart():
+    return render_template("heart.html")
+
+@app.route("/forest")
+def forest():
+    return render_template("forest_fire.html")
+
+@app.route("/iris")
+def iris():
+    return render_template("iris.html")
+
+@app.route("/glass")
+def glass():
+    return render_template("glass.html")
+
+def ValuePredictor(to_predict_list, size):
+    to_predict = np.array(to_predict_list).reshape(1,size)
+    if(size==8):#Diabetes
+        loaded_model = joblib.load("diabetes")
+        result = loaded_model.predict(to_predict)
+    elif(size==11):#Heart
+        loaded_model = joblib.load("heart")
+        result =loaded_model.predict(to_predict)
+    elif(size==4):#Iris
+        loaded_model = joblib.load("iris")
+        result =loaded_model.predict(to_predict)
+    elif(size==9):#Glass
+        loaded_model = joblib.load("glass")
+        result =loaded_model.predict(to_predict)
+    return result[0]
+
+@app.route('/result',methods = ["POST"])
+def result():
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list=list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+        if(len(to_predict_list)==8):#Daiabtes
+            result = ValuePredictor(to_predict_list,8)
+            flag=0
+        elif(len(to_predict_list)==11):#heart
+            result = ValuePredictor(to_predict_list,11)
+            flag=0
+        elif(len(to_predict_list)==4):#iris
+            result = ValuePredictor(to_predict_list,4)
+            flag=1
+        elif(len(to_predict_list)==9):#glass
+            result = ValuePredictor(to_predict_list,9)
+            flag=2
+    if(flag==0):
+        if(int(result)==1):
+            prediction='Sorry!!! Looks like you are Suffering!'
+        else:
+            prediction='Hurray!!! You are Healthy for now. Take Care!' 
+    elif(flag==1):
+        if(int(result)==0):
+            prediction='The variety of Iris is Setosa!'
+        elif(int(result)==1):
+            prediction='The variety of Iris is Versicolor!'
+        else:
+            prediction='The variety of Iris is Virginica!'
+    else:
+        if(int(result)==1):
+            prediction='It is type 1 Glass!'
+        elif(int(result)==2):
+            prediction='It is type 2 Glass!'
+        elif(int(result)==3):
+            prediction='It is type 3 Glass!'
+        elif(int(result)==5):
+            prediction='It is type 5 Glass!'
+        elif(int(result)==6):
+            prediction='It is type 6 Glass!'
+        else:
+            prediction='It is type 7 Glass!'
+    return(render_template("result.html", prediction=prediction))
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final=[np.array(int_features)]
+    prediction=model4.predict_proba(final)
+    output='{0:.{1}f}'.format(prediction[0][1], 2)
+
+    if output>str(0.5):
+        prediction='The Forest is in Danger.\nProbability of fire occuring is {}'.format(output)
+    else:
+        prediction='The Forest is safe.\n Probability of fire occuring is {}'.format(output)
+    return(render_template("result.html", prediction=prediction))
+
+@app.route('/predict_em',methods=['POST'])
+def predict_em():
+    
+    return render_template('result.html', prediction=predict.prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ diabetes.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import warnings
+import pickle
+import joblib
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn import svm
+warnings.filterwarnings("ignore")
+
+#Read Dataset
+data = pd.read_csv("diabetes.csv")
+data = np.array(data)  #Change into numpy array
+
+#Values of X & Y
+X = data[0:, 0:-1]
+y = data[0:, -1]
+
+y = y.astype('int')
+X = X.astype('float')
+
+print(X)
+print(y)
+
+
+#Splitting into Training and Testing data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=5)
+
+#Using different models
+log_reg = LogisticRegression()
+log_reg.fit(X_train, y_train)
+pred1 = log_reg.predict(X_test)
+print("predictions= ",pred1)
+print('The accuracy of the Logistic Regression is = ',accuracy_score(pred1,y_test))
+
+rf=RandomForestClassifier()
+rf.fit(X_train,y_train)
+pred2 = rf.predict(X_test)
+print("predictions= ",pred2)
+print('The accuracy of the Random Forest is = ',accuracy_score(pred2,y_test))
+
+supvec=svm.SVC(kernel='linear')
+supvec.fit(X_train,y_train)
+pred3=supvec.predict(X_test)
+print("predictions= ",pred3)
+print('The accuracy of the svm is',accuracy_score(pred3, y_test))
+
+dt=DecisionTreeClassifier()
+dt.fit(X_train,y_train)
+pred4=dt.predict(X_test)
+print("predictions= ",pred4)
+print('The accuracy of the Decision Tree is',accuracy_score(pred4,y_test))
+
+knn=KNeighborsClassifier()
+knn.fit(X_train,y_train)
+pred5=knn.predict(X_test)
+print("predictions= ",pred5)
+print('The accuracy of the KNeighbors is',accuracy_score(pred5,y_test))
+
+
+# joblib.dump(supvec,"diabetes")
+# pickle.dump(supvec,open('forest.pkl','wb'))
